27a6b9b7b8eece4ae4f7bad6f88ec466e615815c
m-lm <31556425+m-lm@users.noreply.github.com>
2025-01-30 22:23:32 -0600

    move lib jars to Maven; rewrite run bash; resolve dependencies

--- run.bash
+++ run.bash
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
-CORE_JAR="lib/gs-core-2.0.jar"
-SWING_JAR="lib/gs-ui-swing-2.0.jar"
-find . -name "*.java" -print | xargs javac -cp "$CORE_JAR:$SWING_JAR"
-java -cp "$CORE_JAR:$SWING_JAR:." src.core.Main "$@"
+mvn clean compile
+mvn exec:java
--- src/storage/JsonStorage.java
+++ src/storage/JsonStorage.java
@@ -1,9 +1,9 @@
 package src.storage;
 
-//import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 
 public class JsonStorage {
-
+    
 }
